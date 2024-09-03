c379513503fdd80911b4d0300a9b7f98d3b48d30
Diego Peppert <dpeppert@cfi.org.ar>
2024-09-02 22:03:14 -0300

    Correccion de directorio de proyectos

---
+++ LogisticaContainers.Web/Models/ContenedorVM.cs
@@ -0,0 +1,11 @@
+﻿namespace LogisticaContainers.Web.Models
+{
+    public class ContenedorVM
+    {
+        public int IdContenedor { get; set; }
+        public string NumeroSerie { get; set; }
+        public DateTime FechaRegistracion { get; set; }
+        public string Direccion { get; set; }
+
+    }
+}
